3e65a3da9cbd6e3088419c1ba007d3d655eeae18
mindongjun <petanerd@gmail.com>
2017-09-21 20:43:01 +0900

    2017-09-21 lecture4

---
+++ 08_DOM/app.js
@@ -0,0 +1,7 @@
+// document.getElementsByClassName("content");
+
+// var myContentDivs = document.getElementsByClassName("content");
+// var myH2 = myContentDivs[1].getElementsByTagName("h2");
+// myH2.innerHTML = "Thursday"
+
+// //var myPageSubtitle = document.getElementById("page-subtitle");
---
+++ 09_DOM_basic_use/app.js
@@ -0,0 +1,57 @@
+// var myBody = document.getElementsTagName("body");
+// myBody[0].innerHTML;
+
+// myBody[0].innerHTML = "<p>replace all tag!</p>"
+
+// var myTitle = document.getElementById("page-title");
+// myTitle
+
+// myTitle.textContent;
+// myTitle.textContent = "Become more super ultra great!"
+
+
+
+// var linkLion = document.getElementById("lion");
+// linkLion
+
+// linkLion.getAttribute("href");
+// linkLion.getAttribute("class");
+
+// linkLion.setAttribute("class","hyu-lion");
+// linkLion.setAttribute("alt","link_to_likelion");
+
+// linkLion.className;
+// linkLion.className = "link-lion";
+
+// linkLion.href;
+
+// linkLion.style;
+
+
+
+// myTitle.setAttribute("style", "left: 10px;");
+// myTitle.setAttribute("style", "left: 10px;","top: 5px;");
+// myTitle.style.color ="red";
+// myTitle.style.background-color = "yellow";
+
+
+
+// var newLi = document.createElement("li");
+// newLi
+
+// var newA = document.createElement("a");
+// newA
+
+// var list = document.getElementById("content-list").getElementsByTagName("ul")[0]
+// list
+// list.appendChild(newLi);
+// newLi.appendChild(newA);
+// newA.innerHTML = "before friday!";
+
+// list.insertBefore(newLi, menu.getElementsByTagName("li")[0]);
+
+
+
+// var child = list.getElementsByTagName("li")[0];
+// var removed = list.removeChild(child);
+// list.appendChild(removed);
